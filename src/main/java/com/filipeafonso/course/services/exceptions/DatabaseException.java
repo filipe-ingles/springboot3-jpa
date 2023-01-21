45860df11399af3e6ce1f3f8f659514d09b12132
filipe-ingles <filipeafonsoingles@gmail.com>
2023-01-21 13:45:17 -0300

    Exception handling - delete

---
+++ src/main/java/com/filipeafonso/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,10 @@
+package com.filipeafonso.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+	
+}
