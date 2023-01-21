06de5c95431fa92ace7dadde1de37eb7fb92a96c
filipe-ingles <filipeafonsoingles@gmail.com>
2023-01-21 12:42:54 -0300

    Payment, one-to-one association

--- src/main/java/com/filipeafonso/course/config/TestConfig.java
+++ src/main/java/com/filipeafonso/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.filipeafonso.course.entities.Category;
 import com.filipeafonso.course.entities.Order;
 import com.filipeafonso.course.entities.OrderItem;
+import com.filipeafonso.course.entities.Payment;
 import com.filipeafonso.course.entities.Product;
 import com.filipeafonso.course.entities.User;
 import com.filipeafonso.course.entities.enums.OrderStatus;
@@ -76,6 +77,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 		
 	}
 	
